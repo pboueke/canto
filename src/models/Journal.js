f21bad45faf9687e1f0a6420ece697edc4a537ce
Pedro Boueke <phboueke@gmail.com>
2021-09-26 22:02:41 -0300

    Updates Journal key management

--- src/models/Journal.js
+++ src/models/Journal.js
@@ -1,17 +1,35 @@
 import bcrypt from 'react-native-bcrypt';
 import isaac from 'isaac';
 
+bcrypt.setRandomFallback(len => {
+  const buf = new Uint8Array(len);
+
+  return buf.map(() => Math.floor(isaac.random() * 256));
+});
+
 export default class Journal {
   constructor(title, icon, key) {
-    bcrypt.setRandomFallback(len => {
-      const buf = new Uint8Array(len);
-
-      return buf.map(() => Math.floor(isaac.random() * 256));
-    });
-
     this.title = title ?? '';
     this.icon = !icon || icon === '' ? 'book' : icon;
     this.id = 'journal_' + new Date().getMilliseconds();
-    this.key = bcrypt.hashSync(key, 8);
+    this.secure = key !== '';
+    this.hash = bcrypt.hashSync(key, 10);
+
+    if (this.secure) {
+      // in-memory store of private key as array
+      var _secret = [...key];
+      this.storeKey = k => {
+        _secret = [...key];
+      };
+      this.getKey = () => _secret.join('');
+    }
+  }
+
+  unlock(key) {
+    if (!this.secure) {
+      return true;
+    }
+    this.storeKey(key);
+    return bcrypt.compareSync(key, this.hash);
   }
 }
