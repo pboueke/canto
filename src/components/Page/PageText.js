cdc2dccc960f0e2bc0d6767f15dd75ad9459fa68
Pedro Boueke <phboueke@gmail.com>
2021-09-29 20:29:39 -0300

    Added Markdown renderer for PageText

--- src/components/Page/PageText.js
+++ src/components/Page/PageText.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import styled from 'styled-components/native';
+import Markdown from 'react-native-markdown-display';
 
 export default props => (
   <View>
     {props.editMode ? (
-      <PageTextEditor value={props.value} onChangeText={props.onChange} />
+      <PageTextEditor value={props.value} onChange={props.onChange} />
     ) : (
       <PageTextPreview value={props.value} />
     )}
   </View>
 );
 
-const PageTextEditor = props => (
-  <TextEditor
-    value={props.value}
-    multiline={true}
-    placeholder={placeholder}
-    onChangeText={props.onChange}
-    numberOfLines={10}
-  />
+const PageTextEditor = props => {
+  const [text, setText] = useState(props.value);
+  const onChange = val => {
+    setText(val);
+    props.onChange(val);
+  };
+  return (
+    <TextEditor
+      value={text}
+      multiline={true}
+      placeholder={placeholder}
+      onChangeText={onChange}
+      numberOfLines={10}
+    />
+  );
+};
+
+const PageTextPreview = props => (
+  <TextPreview>
+    <Markdown>{props.value}</Markdown>
+  </TextPreview>
 );
 
-const PageTextPreview = props => <TextPreview>{props.value}</TextPreview>;
-
-const TextPreview = styled.Text`
+const TextPreview = styled.View`
   border-width: 2px;
   border-radius: 5px;
   border-style: solid;
@@ -47,11 +59,13 @@ const placeholder = `Write your journal entry here...
 
 Canto uses Markdown to format your text, it's a very practical way to format text. Here are some tips:
 
-# Use # at the start of the line to create a header.
+# Use hashtag at the start of the line to create a header.
 Add asteriscs for styling: **bold text**, *italic text*.
 Create lists by using lines started by * or numbers.
 Markdown also allows for tables, lines, quotes, and links:
 [markdown cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
 
-Formatting will be applied once you save your changes. Also note: The first line of this page will be used for the Page preview. The text contained in it can also be used for search queries. 
+Formatting will be applied once you save your changes. 
+
+Also note: The first line of this page will be used for the Page preview. The text contained in it can also be used for search queries. 
 `;
