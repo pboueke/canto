ecfc817accb0a6e1865a7db6363640e158f854a4
Pedro Boueke <phboueke@gmail.com>
2021-10-01 13:31:28 -0300

    Fixes Filter apply method

--- src/models/Filter.js
+++ src/models/Filter.js
@@ -65,7 +65,7 @@ export default class Filter {
 
     const getDateOnly = date => {
       const d = new Date(date);
-      return new Date((d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate()));
+      return new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate());
     };
 
     const applyDate = (start, end, items) => {
